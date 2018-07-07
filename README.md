eb862a2cedaf1defcfe140d5250913b92266fb2e
Andy Wilde <andy.wilde@sainsburysbank.co.uk>
2018-07-07 08:03:08 +0100

    Set up doctest for size=3

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # primes
-Generates a multiplication table of primes
+Generates a multiplication table of primes.
+
+Run doctest using:
+```sh
+python -m doctest primes.py
+```
+For verbose output use:
+```sh
+python -m doctest -v primes.py
+```
---
+++ primes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def prime_multi_table (num):
+    """
+    Generate multiplication table of primes
+    >>> prime_multi_table(3)
+    '|  | 2| 3| 5|\\n| 2| 4| 6| 10|\\n| 3| 6| 9| 15|\\n| 5| 10| 15| 25|'
+    """
+    output="""|  | 2| 3| 5|
+| 2| 4| 6| 10|
+| 3| 6| 9| 15|
+| 5| 10| 15| 25|"""
+    return output
