ecdf9e241186ce1203756f6b7a2fd04515feee04
Andy Wilde <andy.wilde@sainsburysbank.co.uk>
2018-07-10 08:25:10 +0100

    Fix doctest test broken by table output change

--- primes.py
+++ primes.py
@@ -38,7 +38,10 @@ def prime_multi_table(n):
     """
     Generate multiplication table of primes
     >>> prime_multi_table(3)
-    '|  | 2| 3| 5|\\n| 2| 4| 6| 10|\\n| 3| 6| 9| 15|\\n| 5| 10| 15| 25|'
+    |  | 2| 3| 5|
+    | 2| 4| 6| 10|
+    | 3| 6| 9| 15|
+    | 5| 10| 15| 25|
     """
     primes = get_primes(n)
     # TODO: generate table in more efficient/elegant way
